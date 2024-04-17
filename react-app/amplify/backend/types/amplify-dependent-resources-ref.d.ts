f13b1fb49d8c53afd8bab36185140fbe12fb272d
Waseq225 <mohammadwaseq225@gmail.com>
2024-04-17 18:50:35 +1000

    initial commit

---
+++ react-app/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "reactapp209fef6d": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
